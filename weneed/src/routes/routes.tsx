e304979fd9bd23b3c3bbd05d842bb0db7ecd9eb0
hayoung2 <39898938+hayoung2@users.noreply.github.com>
2025-01-31 05:23:05 +0900

    ENH: signup 연결

--- weneed/src/routes/routes.tsx
+++ weneed/src/routes/routes.tsx
@@ -2,10 +2,12 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { RouterPath } from '@/routes/path'
 import { HomePage } from '@/pages/Home/Home'
 import { LoginPage } from '@/pages/Login/Login'
+import  SignUpPage  from '@/pages/SignUp/SignUp'
 
 const router = createBrowserRouter([
   { path: RouterPath.HOME, element: <HomePage /> },
   { path: RouterPath.LOGIN, element: <LoginPage /> },
+  { path: RouterPath.SIGNUP, element: <SignUpPage /> },
 ])
 
 export const Routes = () => {
