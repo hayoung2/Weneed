1ca42ce63441a78d8ec2e21dbd401c89c370485d
hayoung2 <39898938+hayoung2@users.noreply.github.com>
2025-02-02 03:12:41 +0900

    ENH: header 구현

--- weneed/src/components/common/Header/Header.tsx
+++ weneed/src/components/common/Header/Header.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import logo from "@/assets/icons/logo.svg";
+import styles from "@/components/common/Header/Header.module.scss";
 
 const Header: React.FC = () => {
   return (
-    <header className="header">
-      <h1>로그인 페이지</h1>
+    <header className={styles.header}>
+      <div className={styles.logo}>
+      <a href='/'><img src={logo} alt="Logo" width={150} height={35} className={styles.img} /></a>
+      </div>
+      <div className={styles.menu}>
+        <p className={styles.boldText}>검색 & <span>AI 매칭</span></p>
+        <a href='/introduction'><p className={styles.boldText}>with 위니드</p></a>
+        <a href='/login'><p className={styles.basicText}>로그인</p></a>
+        <a href='/signup'><p className={styles.basicText}>회원가입</p></a>
+      </div>
     </header>
   );
 };
