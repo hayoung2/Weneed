fe1e70137ce691ac50b7aaaa3a30c15734731302
hayoung2 <39898938+hayoung2@users.noreply.github.com>
2025-02-03 03:36:42 +0900

    ENH: 드롭다운 생성

---
+++ weneed/src/components/common/UnitDropdown/UnitDropdown.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import styles from "./UnitDropdown.module.scss";
+import arrowDownIcon from "@/assets/icons/arrowdown.svg"; 
+
+const units = ["단위", "kg", "t", "L"];
+
+interface UnitDropdownProps {
+  value: string;
+  onChange: (unit: string) => void;
+}
+
+const UnitDropdown: React.FC<UnitDropdownProps> = ({ value, onChange }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedUnit, setSelectedUnit] = useState<string>("단위"); // 기본값 "단위"
+
+  const handleSelect = (unit: string) => {
+    setSelectedUnit(unit);
+    onChange(unit);
+    setIsOpen(false);
+  };
+
+  return (
+    <div className={styles.dropdown}>
+      <div
+        className={`${styles.selected} ${isOpen ? styles.focused : ""}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {selectedUnit}
+        <img
+          src={arrowDownIcon}
+          className={isOpen ? styles.arrowUp : styles.arrowDown}
+          alt="Dropdown Arrow"
+        />
+      </div>
+      {isOpen && (
+        <ul className={styles.options}>
+          {units.map((unit) => (
+            <li key={unit} onClick={() => handleSelect(unit)}>
+              {unit}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default UnitDropdown;
