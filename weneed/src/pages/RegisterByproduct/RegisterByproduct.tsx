ec4282febf1b0431ee101bac4e448d6b077b3679
hayoung2 <39898938+hayoung2@users.noreply.github.com>
2025-02-06 01:12:22 +0900

    ENH: 정보 등록 페이지 수정

--- weneed/src/pages/RegisterByproduct/RegisterByproduct.tsx
+++ weneed/src/pages/RegisterByproduct/RegisterByproduct.tsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import styles from "./RegisterByproduct.module.scss";
 import InputBox from "@/components/common/InputBox/InputBox";
 import UnitBigDropdown from "@/components/common/UnitDropdown/UnitBigDropdown";
+import Header from "@/components/common/Header/Header";
+import Footer from "@/components/common/Footer/Footer";
+import FormButton from "@/components/common/FormButton/FormButton";
 
 const RegisterByproduct: React.FC = () => {
   const [formData, setFormData] = useState({ byproductName: "" });
@@ -12,25 +15,37 @@ const RegisterByproduct: React.FC = () => {
   };
 
   return (
-    <div className={styles.container}>
-      <h2 className={styles.formTitle}>공급 가능한 부산물 입력하기</h2>
-      <p className={styles.formSubtitle}>
-        회사에서 공급 가능한 부산물 자원을 입력해주세요.
-      </p>
-      <div className={styles.inputGroup}>
-        <InputBox
-          type="text"
-          placeholder="부산물 자원 이름"
-          value={formData.byproductName}
-          onChange={(e) => handleChange("byproductName", e.target.value)}
-        />
-        <UnitBigDropdown value={selectedUnit} onChange={setSelectedUnit} />
-        <textarea
-          className={styles.textarea}
-          placeholder="부산물 성분 분석(자세하게 적을수록 정확한 매칭이 가능합니다.)"
-        />
+    <>
+      <Header />
+      <div className={styles.container}>
+        <h2 className={styles.formTitle}>공급 가능한 부산물 등록하기</h2>
+        <p className={styles.formSubtitle}>
+          회사에서 공급 가능한 부산물 자원을 입력해주세요.
+        </p>
+        <div className={styles.inputGroup}>
+          <InputBox
+            type="text"
+            placeholder="부산물 자원 이름"
+            value={formData.byproductName}
+            onChange={(e) => handleChange("byproductName", e.target.value)}
+          />
+       
+          <UnitBigDropdown 
+          value={selectedUnit} onChange={setSelectedUnit} />
+          <textarea
+            className={styles.textarea}
+            placeholder="부산물 성분 분석(자세하게 적을수록 정확한 매칭이 가능합니다.)"
+          />
+        </div>
+        <p className={styles.bottomText}>
+          등록한 정보는 마이페이지에서 확인할 수 있어요.
+        </p>
+        <FormButton type="submit" className={styles.formButton}>
+          공급 가능한 부산물 자원 등록하기
+        </FormButton>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
--- weneed/src/pages/RegisterResource/RegisterResource.tsx
+++ weneed/src/pages/RegisterResource/RegisterResource.tsx
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./RegisterResource.module.scss";
+import InputBox from "@/components/common/InputBox/InputBox";
+import UnitBigDropdown from "@/components/common/UnitDropdown/UnitBigDropdown";
+import Header from "@/components/common/Header/Header";
+import Footer from "@/components/common/Footer/Footer";
+import EditButton from "@/components/common/EditButton/EditButton";
 
 const RegisterResource: React.FC = () => {
+  const [formData, setFormData] = useState({ byproductName: "" });
+  const [selectedUnit, setSelectedUnit] = useState("");
+
+  const handleChange = (key: string, value: string) => {
+    setFormData((prev) => ({ ...prev, [key]: value }));
+  };
+
   return (
-    <div className={styles.container}>
-      <h2 className={styles.formTitle}>회사소개 작성하기</h2>
-      <p className={styles.formSubtitle}>
-        위니드 서비스 이용을 위해 정확하게 작성해주세요.
-      </p>
-    </div>
+    <>
+      <Header />
+      <div className={styles.container}>
+        <h2 className={styles.formTitle}>필요 자원 등록하기</h2>
+        <p className={styles.formSubtitle}>
+          회사에서 필요한 자원을 입력해주세요.
+        </p>
+        <div className={styles.inputGroup}>
+          <InputBox
+            type="text"
+            placeholder="자원 이름"
+            value={formData.byproductName}
+            onChange={(e) => handleChange("byproductName", e.target.value)}
+          />
+       
+          <UnitBigDropdown   className={styles.customDropdown} 
+          value={selectedUnit} onChange={setSelectedUnit} />
+        </div>
+        <p className={styles.bottomText}>
+          등록한 정보는 마이페이지에서 확인할 수 있어요.
+        </p>
+        <EditButton type="submit" className={styles.formButton}>
+          필요한 자원 등록하기
+        </EditButton>
+      </div>
+      <Footer />
+    </>
   );
 };
 
