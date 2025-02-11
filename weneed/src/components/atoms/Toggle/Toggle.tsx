14fc4ed2642da6f39d8ab077e91266f793556ce5
hayoung2 <39898938+hayoung2@users.noreply.github.com>
2025-02-12 03:58:42 +0900

    ENH: 개인회원 ai 매칭 ㄴㄴ

--- weneed/src/components/atoms/Toggle/Toggle.tsx
+++ weneed/src/components/atoms/Toggle/Toggle.tsx
@@ -1,4 +1,6 @@
 import styles from '@/components/atoms/Toggle/Toggle.module.scss'
+import { useAuth } from "@/components/contexts/AuthContext"; 
+import { useNavigate } from 'react-router-dom';
 
 interface ToggleSwitchProps {
   checked: boolean;
@@ -6,10 +8,31 @@ interface ToggleSwitchProps {
   onChange: () => void;
 }
 
-const ToggleSwitch: React.FC<ToggleSwitchProps> = ({ checked, style,onChange }) => {
+const ToggleSwitch: React.FC<ToggleSwitchProps> = ({ checked, style, onChange }) => {
+  const { user } = useAuth(); 
+  const isNotLoggedIn = !user; 
+  const isPersonalUser = user?.userType === "개인"; 
+  const navigate = useNavigate();
+  const handleToggle = () => {
+    if (isNotLoggedIn) {
+      alert("로그인이 필요합니다.");
+      navigate('/login');
+      return;
+    }
+    if (isPersonalUser) {
+      alert("개인 회원에게는 AI 매칭이 제공되지 않습니다."); 
+      return;
+    }
+    onChange(); 
+  };
+
   return (
-    <label className={styles.toggleSwitch} style={{...style}}>
-      <input type="checkbox" checked={checked} onChange={onChange} />
+    <label className={styles.toggleSwitch} style={{ ...style }}>
+      <input 
+        type="checkbox" 
+        checked={isNotLoggedIn || isPersonalUser ? false : checked} 
+        onChange={handleToggle} 
+      />
       <span className={styles.slider}></span>
     </label>
   );
